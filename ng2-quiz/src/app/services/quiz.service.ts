978a4539e350d109dd66b2517ab17c7c0aa2398e
gupchirag <gchirag09@gmail.com>
2018-08-13 10:52:58 +0530

    Online Quiz Application in Angular
    Online Quiz Application in Angular

---
+++ ng2-quiz/src/app/services/quiz.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class QuizService {
+
+  constructor(private http: Http) { }
+
+  get(url: string) {
+    return this.http.get(url).map(res => res.text().length > 0 ? res.json() : null);
+  }
+
+  getAll() {
+    return [
+      { id: 'data/c.json', name: 'C Programming' },
+      { id: 'data/cpp.json', name: 'C++ Programming' },
+      { id: 'data/java.json', name: 'Java Programming' }
+    ];
+  }
+
+}
